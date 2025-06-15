a228d6f90316d57656a4871830537ea81b1102f4
Jivan Patil <patiljt11@gmail.com>
2025-06-15 22:28:29 +0530

    my second changes

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,6 @@ class Hello{
 
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Hello 3");
+        System.out.println("Hello 4");
     }
 }
